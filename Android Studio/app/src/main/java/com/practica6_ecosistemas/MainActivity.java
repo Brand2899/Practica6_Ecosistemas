8ab4a43c7ca7277acf2b0ffae59c4fff64234a38
Brand28 <alejandrobrand28@gmail.com>
2021-09-21 17:58:33 -0500

    botones creados

--- Android Studio/app/src/main/java/com/practica6_ecosistemas/MainActivity.java
+++ Android Studio/app/src/main/java/com/practica6_ecosistemas/MainActivity.java
@@ -3,12 +3,21 @@ package com.practica6_ecosistemas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button bnCenter, bnLeft, bnRight, bnUp, bnDown;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bnCenter = findViewById(R.id.bnCenter);
+        bnLeft = findViewById(R.id.bnLeft);
+        bnRight = findViewById(R.id.bnRight);
+        bnUp = findViewById(R.id.bnUp);
+        bnDown = findViewById(R.id.bnDown);
     }
 }

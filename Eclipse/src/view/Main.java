ceac2a52fcdf9fb839953650854ae5c08557da51
Brand28 <alejandrobrand28@gmail.com>
2021-09-21 18:22:03 -0500

    re crear eclipse

--- Eclipse/src/view/Main.java
+++ Eclipse/src/view/Main.java
@@ -1,10 +1,19 @@
 package view;
 
-public class Main {
+import processing.core.PApplet;
+
+public class Main extends PApplet {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		PApplet.main(Main.class.getName());
 	}
-
+	
+	public void settings() {
+		size(1500, 800);
+	}
+	
+	public void draw() {
+		background(255);
+	}
 }
